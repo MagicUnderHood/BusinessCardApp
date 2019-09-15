3bfdb3da51971c0777e38ecc933ce12a5095f7f5
Tanya <tanya.orlova17@gmail.com>
2019-09-15 20:59:05 +0300

    Added click button listener

--- app/src/main/java/com/tanyaorlova/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/tanyaorlova/businesscardapp/MainActivity.kt
@@ -1,12 +1,69 @@
 package com.tanyaorlova.businesscardapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sendMessageButton: Button
+    private lateinit var githubButton: ImageButton
+    private lateinit var linkedInButton: ImageButton
+    private lateinit var telegramButton: ImageButton
+    private lateinit var messageTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        messageTextView = findViewById(R.id.editTextEnterMessage)
+
+        sendMessageButton = findViewById(R.id.bttnSendImage)
+        sendMessageButton.setOnClickListener { sendingMessage() }
+
+        githubButton = findViewById(R.id.bttnGithub)
+        githubButton.setOnClickListener{openGithub()}
+
+        linkedInButton = findViewById(R.id.bttnLinkedIn)
+        linkedInButton.setOnClickListener { openLinkedIn() }
+
+        telegramButton = findViewById(R.id.bttnTelegram)
+        telegramButton.setOnClickListener { openTelegram() }
     }
+
+    private fun openTelegram() {
+        val uri = Uri.parse("https://t.me/TanyaOrlova")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+    }
+
+    private fun openLinkedIn() {
+        val uri = Uri.parse("https://www.linkedin.com/in/tanyaorlova/")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+    }
+
+    private fun openGithub() {
+        val uri = Uri.parse("https://github.com/TanyaOrlova")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+    }
+
+    private fun sendingMessage() {
+        val intent = Intent(
+            Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", "tanya.orlova17@gmail.com", null
+            )
+        )
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Message from Business card app")
+        intent.putExtra(Intent.EXTRA_TEXT, messageTextView.text)
+        startActivity(Intent.createChooser(intent, getString(R.string.chooseEmailClient)))
+    }
+
+
 }
