f54f6543e83ae9fc0778fb537321e34f4ca2439b
Tanya <tanya.orlova17@gmail.com>
2019-09-22 18:13:43 +0300

    Update MainActivity.kt
    Refactored code style

--- app/src/main/java/com/tanyaorlova/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/tanyaorlova/businesscardapp/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         sendMessageButton.setOnClickListener { sendingMessage() }
 
         val githubButton: ImageButton = findViewById(R.id.bttnGithub)
-        githubButton.setOnClickListener{ openLink(getString(R.string.my_github)) }
+        githubButton.setOnClickListener { openLink(getString(R.string.my_github)) }
 
         val linkedInButton: ImageButton = findViewById(R.id.bttnLinkedIn)
         linkedInButton.setOnClickListener { openLink(getString(R.string.my_linkedin)) }
@@ -35,8 +35,10 @@ class MainActivity : AppCompatActivity() {
 
         val disclaimerTextView = TextView(this)
         disclaimerTextView.layoutParams =
-            LinearLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT)
+            LinearLayout.LayoutParams(
+                ActionBar.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+            )
         disclaimerTextView.text = getString(R.string.disclaimer)
         disclaimerTextView.gravity = Gravity.END
         disclaimerTextView.setPadding(0, 0, 16, 0)
@@ -44,13 +46,12 @@ class MainActivity : AppCompatActivity() {
         activityLayout.addView(disclaimerTextView)
     }
 
-    private fun openLink(link: String){
+    private fun openLink(link: String) {
         val uri = Uri.parse(link)
         val intent = Intent(Intent.ACTION_VIEW, uri)
         if (intent.resolveActivity(packageManager) != null) {
             startActivity(intent)
-        }
-        else{
+        } else {
             Toast.makeText(this, R.string.error_no_link_app, Toast.LENGTH_LONG).show()
         }
     }
@@ -63,13 +64,10 @@ class MainActivity : AppCompatActivity() {
         )
         intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.messageSubject))
         intent.putExtra(Intent.EXTRA_TEXT, messageTextView.text)
-        if (intent.resolveActivity(packageManager) != null){
+        if (intent.resolveActivity(packageManager) != null) {
             startActivity(Intent.createChooser(intent, getString(R.string.chooseEmailClient)))
-        }
-        else{
+        } else {
             Toast.makeText(this, R.string.error_no_email_app, Toast.LENGTH_LONG).show()
         }
     }
-
-
 }
